f57bddd6916bbc208ba5717d0b36543eca7f9cc1
Soko Paul <sopapaso73@gmail.com>
2018-08-31 23:47:15 +0300

    edit reception.py read data from vip list and return list [Delivers #160201694]

--- registration/reception/reception.py
+++ registration/reception/reception.py
@@ -2,6 +2,12 @@
 
 """
 
-def get_vip_list():
 
-    pass
+def get_vip_list():
+    vip_file_data = open('../data/vip_list.txt', 'r')
+    vip_list = []
+    for name in vip_file_data.readlines():
+        if name is not None:
+            vip_list.append({"name": name.rstrip('\n'), "category": "VIP"})
+    vip_file_data.close()
+    return vip_list
