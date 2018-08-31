9ef763037df81ff2201b564b444935bd5fbfaa10
Soko Paul <sopapaso73@gmail.com>
2018-08-31 23:50:36 +0300

    edit reception.py to add get_ordinary_list function [Delivers #160201792]

--- registration/reception/reception.py
+++ registration/reception/reception.py
@@ -11,3 +11,12 @@ def get_vip_list():
             vip_list.append({"name": name.rstrip('\n'), "category": "VIP"})
     vip_file_data.close()
     return vip_list
+
+
+"""Reads the ordinary list text file and returns a list of registered users
+
+"""
+
+
+def get_ordinary_list():
+    pass
