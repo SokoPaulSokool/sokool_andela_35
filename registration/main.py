65facbbdbc4c5e32a5ea354339b131b866306c25
Soko Paul <sopapaso73@gmail.com>
2018-08-31 19:04:45 +0300

    edit dummy data [(Delivers #160200945]

--- registration/main.py
+++ registration/main.py
@@ -2,7 +2,7 @@
 
 
 def registration_checker(name):
-    vip_list = [{name: 'paul', category: "VIP"},
-                {name: 'peter', category: "VIP"}]
-    ordinary_list = [{name: 'paul', category: "ORDINARY"},
-                     {name: 'peter', category: "ORDINARY"}]
+    vip_list = [{'name': 'paul', 'category': "VIP"},
+                {'name': 'peter', 'category': "VIP"}]
+    ordinary_list = [{'name': 'paul', 'category': "ORDINARY"},
+                     {'name': 'peter', 'category': "ORDINARY"}]
