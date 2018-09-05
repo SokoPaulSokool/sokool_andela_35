e92dc3c1224a958492d5b9a36bce2581c677b81a
Soko Paul <sopapaso73@gmail.com>
2018-09-05 13:46:26 +0300

    add add test for adding user

---
+++ signup/signup.py
@@ -0,0 +1,9 @@
+class SignUp():
+    
+    def __init__(self):
+        """initialize user bio dictionary"""
+        self.user_bio = dict()
+
+    def add(self, username, email, password):
+        """add user to user_bio with username as key"""
+        self.user_bio[username] = {password:password, email:email}
--- signup/test_signup.py
+++ signup/test_signup.py
@@ -5,4 +5,10 @@ from signup import SignUp
 class SignUpTest(unittest.TestCase):
 
     def setUp(self):
+        """set up signup"""
         self.signup = SignUp()
+
+    def test_add_user(self):
+        """test adding user"""
+        self.signup.add("John Doe", "sopapaso73@gmail.com", "12345")
+        self.assertEqual(len(self.signup.user_bio), 1)
