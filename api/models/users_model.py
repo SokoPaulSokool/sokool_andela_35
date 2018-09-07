c0872058d9ae7f164c2bb590317e461002ae9072
Soko Paul <sopapaso73@gmail.com>
2018-09-07 12:46:28 +0300

    add user model validators and tests

--- api/models/users_model.py
+++ api/models/users_model.py
@@ -1,9 +1,29 @@
+from re import match
+
 
 class User:
     def __init__(self, username, email, password):
         self.username = username
         self.email = email
         self.password = password
+
+    def is_validName(self):
+        if self.username.isalpha():
+            return True
+        return False
+
+    def is_validEmail(self):
+        if bool(
+            match(
+                r"^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$",
+                self.email)):
+            return True
+        return False
+
+    def is_valid_user(self):
+        if self.is_validEmail() and self.is_validName():
+            return True
+        return False
 
 
 class UserList:
--- tests/test_users_model.py
+++ tests/test_users_model.py
@@ -11,3 +11,16 @@ class TestUser(unittest.TestCase):
 
     def test_user_instance(self):
         self.assertIsInstance(self.user, User)
+
+    def test_valid_user_inputs(self):
+        self.assertEqual(self.user.is_valid_user(), True)
+
+    def test_invalid_email(self):
+        invalid_email_user = User('soko', 'paul', '4321')
+        self.assertEqual(invalid_email_user.is_validEmail(), False)
+        self.assertEqual(invalid_email_user.is_valid_user(), False)
+
+    def test_invalid_name(self):
+        invalid_name_user = User('soko@paul', 'paul', '4321')
+        self.assertEqual(invalid_name_user.is_validName(), False)
+        self.assertEqual(invalid_name_user.is_valid_user(), False)
